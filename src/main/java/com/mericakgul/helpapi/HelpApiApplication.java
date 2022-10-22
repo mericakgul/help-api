69f917e156daee88d35b647f83e8bf1a7498b4b4
Meriç Akgül <39882155+mericakgul@users.noreply.github.com>
2022-10-23 00:12:48 +0200

    initial commit

---
+++ src/main/java/com/mericakgul/helpapi/HelpApiApplication.java
@@ -0,0 +1,13 @@
+package com.mericakgul.helpapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelpApiApplication.class, args);
+    }
+
+}
